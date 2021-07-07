ca63bcb03a3ae41feb6200dd812f250ebb5d1471
Orizin <nlgporizin10@gmail.com>
2021-07-08 08:50:20 +0900

    ゲストログイン機能の修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,19 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: %i[update destroy]
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :ensure_normal_user, only: %i[update destroy]
 
-  def ensure_normal_user
-    redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。" if resource.email == "guest@example.com"
-  end
+    def update; end
 
-  protected
+    def destroy; end
 
-  def after_update_path_for(_resource)
-    mypage_path(current_user)
+    def ensure_normal_user
+      redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。" if resource.email == "guest@example.com"
+    end
+
+    protected
+
+    def after_update_path_for(_resource)
+      mypage_path(current_user)
+    end
   end
 end
