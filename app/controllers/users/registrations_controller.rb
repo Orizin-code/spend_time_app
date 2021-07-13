e59d52af00ab76ab5514b817ceaf715593fa217e
Orizin <nlgporizin10@gmail.com>
2021-07-14 00:23:34 +0900

    タイポ修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: %i[update destroy]
 
- feature/gest-login
     def update
       super
       update_internal
@@ -16,7 +15,6 @@ module Users
     def update_internal; end
 
     def destroy_internal; end
-
 
     def ensure_normal_user
       redirect_to root_path, alert: "ゲストユーザーの更新・削除はできません。" if resource.email == "guest@example.com"
