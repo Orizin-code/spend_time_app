18f087c1804969af56b6d6eaf42b0f8a180b36ff
Orizin <nlgporizin10@gmail.com>
2021-08-17 17:12:57 +0900

    ログイン必須機能の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[show]
+  before_action :authenticate_user!
   before_action :set_post, only: %i[edit update destroy]
 
   # 1ページの表示数
