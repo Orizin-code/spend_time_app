b25e0da0dd0db24bc84eeba90096be3a5ee67c5c
Orizin <nlgporizin10@gmail.com>
2021-08-28 19:30:38 +0900

    Userモデルのバリデーション修正/Rspec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # バリデーション
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :username, presence: true, length: { in: 1..16, allow_blank: true }, uniqueness: { case_sensitive: true }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { in: 8..16, allow_blank: true },
+                       format: { with: /\A[a-zA-Z\d@\-_]+\z/, allow_blank: true,
+                                 message: "で利用できるのは、半角英数字および記号(@, -, _)のみです。" }
+
+  # ゲストログイン
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
